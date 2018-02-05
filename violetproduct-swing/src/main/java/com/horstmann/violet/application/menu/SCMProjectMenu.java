57f7004187bfd3fd550e70c6bf55b7c70350a69d
gagan <singhgagan008@gmail.com>
2018-02-05 17:25:48 -0500

    removed one class

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/SCMProjectMenu.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/SCMProjectMenu.java
@@ -41,7 +41,6 @@ import com.horstmann.violet.framework.injection.resources.annotation.ResourceBun
 import com.horstmann.violet.workspace.IWorkspace;
 import com.horstmann.violet.workspace.editorpart.IEditorPart;
 import com.horstmann.violet.workspace.editorpart.IEditorPartBehaviorManager;
-import com.horstmann.violet.workspace.editorpart.behavior.CheckAssociationAndCompositionOnOneClassBehavior;
 import com.horstmann.violet.workspace.editorpart.behavior.UndoRedoCompoundBehavior;
 
 /**
