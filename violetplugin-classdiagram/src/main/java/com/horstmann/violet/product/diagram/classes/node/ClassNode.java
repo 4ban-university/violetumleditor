7dc32fcd1b58841f78d0c45bf42d73e08d0f8301
Bill Chen <bill1119@hotmail.com>
2018-02-11 23:22:25 -0500

    CBO showing is done

--- violetplugin-classdiagram/src/main/java/com/horstmann/violet/product/diagram/classes/node/ClassNode.java
+++ violetplugin-classdiagram/src/main/java/com/horstmann/violet/product/diagram/classes/node/ClassNode.java
@@ -3,7 +3,8 @@ package com.horstmann.violet.product.diagram.classes.node;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import com.horstmann.violet.framework.graphics.Separator;
 import com.horstmann.violet.framework.graphics.content.*;
 import com.horstmann.violet.framework.graphics.content.VerticalLayout;
@@ -15,12 +16,12 @@ import com.horstmann.violet.product.diagram.abstracts.node.ColorableNode;
 import com.horstmann.violet.product.diagram.abstracts.node.INode;
 import com.horstmann.violet.product.diagram.property.text.MultiLineText;
 import com.horstmann.violet.product.diagram.property.text.SingleLineText;
-
 /**
  * A class node in a class diagram.
  */
 public class ClassNode extends ColorableNode
 {
+	
 	/**
      * Construct a class node with a default size
      */
@@ -31,7 +32,9 @@ public class ClassNode extends ColorableNode
         name.setAlignment(LineText.CENTER);
         attributes = new MultiLineText(PROPERTY_CONVERTER);
         methods = new MultiLineText(PROPERTY_CONVERTER);
+        couplingBetweenObjects = new SingleLineText(NAME_CONVERTER);
         createContentStructure();
+        
     }
 
     protected ClassNode(ClassNode node) throws CloneNotSupportedException
@@ -40,8 +43,12 @@ public class ClassNode extends ColorableNode
         name = node.name.clone();
         attributes = node.attributes.clone();
         methods = node.methods.clone();
+        couplingBetweenObjects = node.couplingBetweenObjects.clone();
         createContentStructure();
+        showCouplingBetweenObjects();
     }
+    
+
 
     @Override
     protected void beforeReconstruction()
@@ -60,10 +67,17 @@ public class ClassNode extends ColorableNode
         {
             methods = new MultiLineText();
         }
+        if(null == couplingBetweenObjects)
+        {
+        	couplingBetweenObjects = new SingleLineText();
+        }
+       
         name.reconstruction(NAME_CONVERTER);
         attributes.reconstruction(PROPERTY_CONVERTER);
         methods.reconstruction(PROPERTY_CONVERTER);
         name.setAlignment(LineText.CENTER);
+        couplingBetweenObjects.reconstruction(NAME_CONVERTER);
+        couplingBetweenObjects.setAlignment(LineText.LEFT);
     }
 
     @Override
@@ -75,16 +89,20 @@ public class ClassNode extends ColorableNode
     @Override
     protected void createContentStructure()
     {
+    	TextContent couplingBetweenObjectsContent = new TextContent(couplingBetweenObjects);
         TextContent nameContent = new TextContent(name);
         nameContent.setMinHeight(MIN_NAME_HEIGHT);
         nameContent.setMinWidth(MIN_WIDTH);
         TextContent attributesContent = new TextContent(attributes);
         TextContent methodsContent = new TextContent(methods);
-
+        
+        
         VerticalLayout verticalGroupContent = new VerticalLayout();
         verticalGroupContent.add(nameContent);
         verticalGroupContent.add(attributesContent);
         verticalGroupContent.add(methodsContent);
+        verticalGroupContent.add(couplingBetweenObjectsContent);
+        
         separator = new Separator.LineSeparator(getBorderColor());
         verticalGroupContent.setSeparator(separator);
 
@@ -113,6 +131,8 @@ public class ClassNode extends ColorableNode
         name.setTextColor(textColor);
         attributes.setTextColor(textColor);
         methods.setTextColor(textColor);
+        couplingBetweenObjects.setTextColor(textColor);
+    
         super.setTextColor(textColor);
     }
 
@@ -138,10 +158,53 @@ public class ClassNode extends ColorableNode
      * @return the class name
      */
     public LineText getName()
-    {
-        return name;
+    {		
+       return name;
     }
+    
+    /**
+     * Gets the value of couplingBetweenObjects and showing it automatically.
+     */
+    public void showCouplingBetweenObjects() 
+    {
+    	new Thread( () ->{
+    		while (true) {
+    			Pattern _preString = Pattern.compile("(.*)CBO:");
+    			Matcher nameMatch = _preString.matcher(couplingBetweenObjects.toString());
+    			String result = couplingBetweenObjects.toString();
+    			String _counting = "CBO" + ':' + (String.valueOf(this.getConnectedEdges().size()));
 
+    			if (nameMatch.find()) {
+    				result = nameMatch.group(1) + _counting;
+
+    			} else {
+    				result = result.concat(_counting);
+    			}
+    			couplingBetweenObjects.setText(result);
+    		}
+    	}).start();
+    }
+    
+    /**
+     * Sets the couplingBetweenObjects property value.
+     * 
+     * @param newValue the couplingBetweenObjects of this class
+     */
+    public void setCouplingBetweenObjects(LineText newValue)
+    {
+    	couplingBetweenObjects.setText(newValue);	
+    }
+    
+    /**
+     * Gets the couplingBetweenObjects property value.
+     * 
+     * @param newValue the couplingBetweenObjects of this class
+     */
+    public LineText getCouplingBetweenObjects()
+    {
+    	return couplingBetweenObjects;
+    }
+    
     /**
      * Sets the attributes property value.
      * 
@@ -181,17 +244,22 @@ public class ClassNode extends ColorableNode
     {
         return methods;
     }
-
+ 
+   
+    
+    
+    
     private SingleLineText name;
     private MultiLineText attributes;
     private MultiLineText methods;
-
-    private transient Separator separator;
-
+    private SingleLineText couplingBetweenObjects;
+  
+    private transient Separator separator = null;
+   
     private static final int MIN_NAME_HEIGHT = 45;
     private static final int MIN_WIDTH = 100;
     private static final String STATIC = "<<static>>";
-    private static final String ABSTRACT = "«abstract»";
+    private static final String ABSTRACT = "Â«abstractÂ»";
     private static final String[][] SIGNATURE_REPLACE_KEYS = {
             { "public ", "+ " },
             { "package ", "~ " },
@@ -201,43 +269,43 @@ public class ClassNode extends ColorableNode
     };
 
     private static final List<String> STEREOTYPES = Arrays.asList(
-            "«Utility»",
-            "«Type»",
-            "«Metaclass»",
-            "«ImplementationClass»",
-            "«Focus»",
-            "«Entity»",
-            "«Control»",
-            "«Boundary»",
-            "«Auxiliary»",
+            "Â«UtilityÂ»",
+            "Â«TypeÂ»",
+            "Â«MetaclassÂ»",
+            "Â«ImplementationClassÂ»",
+            "Â«FocusÂ»",
+            "Â«EntityÂ»",
+            "Â«ControlÂ»",
+            "Â«BoundaryÂ»",
+            "Â«AuxiliaryÂ»",
             ABSTRACT
     );
-
-    private static final LineText.Converter NAME_CONVERTER = new LineText.Converter()
+    
+    public LineText.Converter NAME_CONVERTER = new LineText.Converter()
     {
-        @Override
-        public OneLineText toLineString(String text)
-        {
-            OneLineText controlText = new OneLineText(text);
-            OneLineText lineString = new LargeSizeDecorator(controlText);
+    	@Override
+    	public OneLineText toLineString(String text)
+    	{
+    		OneLineText controlText = new OneLineText(text);
+    		OneLineText lineString = new LargeSizeDecorator(controlText);
 
-            if(controlText.contains(ABSTRACT))
-            {
-                lineString = new ItalicsDecorator(lineString);
-            }
+    		if(controlText.contains(ABSTRACT))
+    		{
+    			lineString = new ItalicsDecorator(lineString);
+    		}
 
-            for(String stereotype : STEREOTYPES)
-            {
-                if(controlText.contains(stereotype))
-                {
-                    lineString = new PrefixDecorator(new RemoveSentenceDecorator(
-                            lineString, stereotype), String.format("<center>%s</center>", stereotype)
-                    );
-                }
-            }
+    		for(String stereotype : STEREOTYPES)
+    		{
+    			if(controlText.contains(stereotype))
+    			{
+    				lineString = new PrefixDecorator(new RemoveSentenceDecorator(
+    						lineString, stereotype), String.format("<center>%</center>", stereotype)
+    						);
+    			}
+    		}
+    		return lineString;
+    	} 
 
-            return lineString;
-        }
     };
     private static final LineText.Converter PROPERTY_CONVERTER = new LineText.Converter()
     {
@@ -257,5 +325,5 @@ public class ClassNode extends ColorableNode
 
             return lineString;
         }
-    };
+    }; 
 }
